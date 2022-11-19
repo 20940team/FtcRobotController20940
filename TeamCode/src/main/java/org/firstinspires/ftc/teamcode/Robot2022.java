f7f43b3446db228e497b3c85fb8958249cf5a975
20940team <vbzxc7293@gmail.com>
2022-11-19 21:57:57 +0300

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2022.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2022.java
@@ -18,7 +18,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 public class Robot2022 extends Robot {
     DcMotor RF, LF, LB, RB, UP;
     Servo grab;
-    BNO055IMU imu;
 
     Robot2022(HardwareMap hardwareMap, Telemetry telemetry, LinearOpMode linearOpMode) {
         super(hardwareMap, telemetry, linearOpMode);
@@ -29,22 +28,6 @@ public class Robot2022 extends Robot {
         UP = hardwareMap.get(DcMotor.class, "upw");
         grab = hardwareMap.get(Servo.class, "grab");
 
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters(); //Акселерометра
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
-        while (!imu.isGyroCalibrated()) { //Калибровка акселерометра
-            delay(30);
-            telemetry.addData("Wait", "Calibration"); //Сообщение о калибровке
-            telemetry.update();
-        }
-        telemetry.addData("Done!", "Calibrated"); //Сообщение об окончании калибровки
-        telemetry.update();
     }
 
 
@@ -85,70 +68,6 @@ public class Robot2022 extends Robot {
         RB.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.left_trigger + gamepad1.right_trigger);
     }
 
-    double getAngle() { //Функция получения данных с акселерометра
-        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-        Acceleration gravity = imu.getGravity();
-        return angles.firstAngle;
-    }
-
-    public void rotate(double degrees) {
-        double pw = 1;
-        double Er0 = -degrees;
-        double errorFix=0;
-        double ErLast = 0;
-        if (Er0 > 0) { pw = -1; }
-        degrees = getAngle() - degrees;
-        if (degrees < -180) {
-            //degrees += 360;
-            Er0 = Er0 * -1;
-            pw *= -1;
-            errorFix=1;
-        }
-        if (degrees > 180) {
-            //degrees -= 360;
-            Er0 = Er0 * -1;
-            pw *= -1;
-            errorFix=2;
-        }
-        while ( Math.abs(degrees - getAngle()) > 3  && linearOpMode.opModeIsActive()) {
-            if (getAngle() > 0 && errorFix==1) { Er0 = Er0 * -1; degrees += 360; pw *= -1; errorFix=0; }
-            if (getAngle() < 0 && errorFix==2) { Er0 = Er0 * -1; degrees -= 360; pw *= -1; errorFix=0; }
-
-            double Er = degrees - (getAngle());
-
-            double kr = -0.4;
-            double Rele = kr * Math.signum(Er);
-
-            ErLast = Er;
-
-
-            double pwf = Rele; //Регулятор
-
-
-            LB.setPower(pwf);
-            LF.setPower(-pwf);
-            RB.setPower(-pwf);
-            RF.setPower(pwf);
-
-                /*telemetry.addData("degrees", degrees);
-                telemetry.addData("getAngle()", getAngle());
-                telemetry.addData("Er (degrees - getAngle)", Er);
-                telemetry.addData("Er0", Er0);
-                telemetry.addData("kp", kp);
-                telemetry.addData("P", P);
-                telemetry.addData("D", D);
-                telemetry.addData("Rele", Rele);
-                telemetry.addData("pw", pw);
-                telemetry.addData("pwf", pwf);
-                telemetry.update();*/
-
-        }
-        LB.setPower(0);
-        LF.setPower(0);
-        RB.setPower(0);
-        RF.setPower(0);
-        delay(500);
-}
 
     public void GoTimer(double x, double y, double time) {
         LF.setPower(y - x);
